d2b53491d7621c6265e17916956baadeabba76dd
Vishal Singh Thakur <thakurvishal1990@gmail.com>
2015-04-06 08:48:29 +0000

    changed css,html,templates

--- js/routes.js
+++ js/routes.js
@@ -6,18 +6,18 @@
 define(['./app'], function (app) {
     'use strict';
     return app.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view1', {
-            templateUrl: 'partials/partial1.html',
+        $routeProvider.when('/dashboard', {
+            templateUrl: 'partials/dashboard.html',
             controller: 'MyCtrl1'
         });
 
-        $routeProvider.when('/view2', {
-            templateUrl: 'partials/partial2.html',
+        $routeProvider.when('/landing', {
+            templateUrl: 'partials/landing.html',
             controller: 'MyCtrl2'
         });
 
         $routeProvider.otherwise({
-            redirectTo: '/view1'
+            redirectTo: '/dashboard'
         });
     }]);
 });
